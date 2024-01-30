6d5171c0de7f61c26bc2ab117eb78afe897c78d8
bloeys <bloeys@outlook.com>
2024-01-30 04:49:37 +0400

    Bism Allah starting tokenizer

--- regexl.go
+++ regexl.go
@@ -1,10 +1,40 @@
 package regexl
 
+var (
+	IsVerbose = false
+)
+
+func (t *Token) IsEmpty() bool {
+	return t == nil || (t.Type == TokenType_Unknown && t.Val == "")
+}
+
+type AstNode struct {
+	Type TokenType
+	Val  string
+
+	Left  *AstNode
+	Right *AstNode
+}
+
 type Regexl struct {
 	Query string
 }
 
-func (r *Regexl) Compile() error {
+func (rl *Regexl) Compile() error {
 
 	return nil
+}
+
+func NewRegexl(query string) (*Regexl, error) {
+
+	rl := &Regexl{
+		Query: query,
+	}
+
+	err := rl.Compile()
+	if err != nil {
+		return nil, err
+	}
+
+	return rl, nil
 }
